0dfb62473ed79e62d5031f23fdd50a05d45d24e2
Pier Fumagalli <pier@usrz.com>
2021-01-25 23:30:24 +0100

    Cleanup and keep logs (but commented out)

--- platforms/ios/src/BarcodeMetadataObjectsDelegate.m
+++ platforms/ios/src/BarcodeMetadataObjectsDelegate.m
@@ -17,6 +17,7 @@
 {
     self = [super init];
     self.callback = callback;
+    // NSLog(@"BarcodeMetadataObjectsDelegate initialized");
     return self;
 }
 
@@ -24,13 +25,15 @@
     :(nonnull AVCaptureMetadataOutput *)output
 {
     [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    // NSLog(@"BarcodeMetadataObjectsDelegate setup correctly");
 }
 
 - (void)captureOutput
-    :(AVCaptureOutput *)captureOutput didOutputMetadataObjects
-    :(NSArray *)metadataObjects fromConnection
-    :(AVCaptureConnection *)connection
+    :(AVCaptureOutput *)captureOutput
+    didOutputMetadataObjects:(NSArray *)metadataObjects
+    fromConnection:(AVCaptureConnection *)connection
 {
+    // NSLog(@"BarcodeMetadataObjectsDelegate %lu objects", metadataObjects.count);
     for (AVMetadataObject *current in metadataObjects) {
         if ([current isKindOfClass:[AVMetadataMachineReadableCodeObject class]]) {
             NSString *text = [(AVMetadataMachineReadableCodeObject *) current stringValue];
